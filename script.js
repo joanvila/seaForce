13345b7a14746954d2bd8d93eb58652ec634578d
estevevaio <esteve.torrents@est.fib.upc.edu>
2016-02-21 00:31:14 +0100

    hardcode ++

--- script.js
+++ script.js
@@ -16,20 +16,34 @@ Leap.loop(controllerOptions, function(frame) {
             var hand = frame.hands[i];
             var x = hand.palmPosition[0];
             var y = hand.palmPosition[1];
+            var rand = getRandomArbitrary(50,window.innerHeight - 150);
             x += 350;
             y -= 350;
             x *= 2;
             y *= 2;
             if (position >= window.innerWidth) {
             	position = 0;
-            	var rand = getRandomArbitrary(50,window.innerHeight - 150);
             	$("#wallBottom").css({height:rand});
             	$("#wallTop").css({height:window.innerHeight - rand - 150});
             }
-            else position += 10;
+            else position += 1;
             $("#square").css({left:x,top:-y});
             $("#wallTop").css({right:position});
             $("#wallBottom").css({right:position});
+            var leftWall  = window.innerWidth - position-70;
+            var rightWall = window.innerWidth - position;
+            var topWall  = $("#wallBottom").offset().top + 150;
+            var botWall = $("#wallBottom").offset().top;
+            //console.log(topWall + ' ' + botWall + ' ' + y);
+            //console.log(leftWall + ' ' + rightWall);
+            if (x+25 >= leftWall && x <= rightWall+25) {
+            	var posPeix = $("#square").offset().top + 25;
+            	//console.log(topWall + ' ' + botWall + ' ' + posPeix);
+            	if (posPeix + 25 >= botWall || posPeix + 25 <= topWall) {
+            		alert("SUCH FAIL!!");
+            		console.log(topWall + ' ' + botWall + ' ' + posPeix + ' ' + leftWall + ' ' + rightWall + ' ' + x);
+            	}
+            }
         }
     }
 
