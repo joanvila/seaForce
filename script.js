540d2ac79ea46909a0bae02e251690b5689388e6
estevevaio <esteve.torrents@est.fib.upc.edu>
2016-02-21 01:52:37 +0100

    super hardcode

--- script.js
+++ script.js
@@ -4,46 +4,48 @@ var previousFrame = null;
 // Setup Leap loop with frame callback function
 var controllerOptions = {enableGestures: true};
 
-var position = 0;
+var position = window.innerWidth;
 
+var acceleration = 5;
+var points = 0;
 // to use HMD mode:
 // controllerOptions.optimizeHMD = true;
 
 Leap.loop(controllerOptions, function(frame) {
     // Display Hand object data
     if (frame.hands.length > 0) {
-        for (var i = 0; i < frame.hands.length; i++) {
-            var hand = frame.hands[i];
-            var x = hand.palmPosition[0];
-            var y = hand.palmPosition[1];
-            var rand = getRandomArbitrary(50,window.innerHeight - 150);
-            x += 350;
-            y -= 350;
-            x *= 2;
-            y *= 2;
-            if (position >= window.innerWidth) {
-            	position = 0;
-            	$("#wallBottom").css({height:rand});
-            	$("#wallTop").css({height:window.innerHeight - rand - 150});
-            }
-            else position += 1;
-            $("#square").css({left:x,top:-y});
-            $("#wallTop").css({right:position});
-            $("#wallBottom").css({right:position});
-            var leftWall  = window.innerWidth - position-70;
-            var rightWall = window.innerWidth - position;
-            var topWall  = $("#wallBottom").offset().top + 150;
-            var botWall = $("#wallBottom").offset().top;
-            //console.log(topWall + ' ' + botWall + ' ' + y);
-            //console.log(leftWall + ' ' + rightWall);
-            if (x+25 >= leftWall && x <= rightWall+25) {
-            	var posPeix = $("#square").offset().top + 25;
-            	//console.log(topWall + ' ' + botWall + ' ' + posPeix);
-            	if (posPeix + 25 >= botWall || posPeix + 25 <= topWall) {
-            		alert("SUCH FAIL!!");
-            		console.log(topWall + ' ' + botWall + ' ' + posPeix + ' ' + leftWall + ' ' + rightWall + ' ' + x);
-            	}
-            }
+		++points;
+		$("#points").text(points);
+        var hand = frame.hands[0];
+        var x = hand.palmPosition[0];
+        var y = hand.palmPosition[1];
+        var rand = getRandomArbitrary(50,window.innerHeight - 150);
+        x += 350;
+        y -= 350;
+        x *= 2;
+        y *= 2;
+        if (position >= window.innerWidth) {
+        	position = 0;
+        	$("#wallBottom").css({height:rand});
+        	$("#wallTop").css({height:window.innerHeight - rand - 150});
+        }
+        else position += acceleration;
+        $("#square").css({left:x,top:-y});
+        $("#wallTop").css({right:position});
+        $("#wallBottom").css({right:position});
+        var leftWall  = window.innerWidth - position-70;
+        var rightWall = window.innerWidth - position;
+        var topWall  = parseInt($("#wallBottom").offset().top) - 150;
+        var botWall = parseInt($("#wallBottom").offset().top);
+
+        if (x+50 >= leftWall && x <= rightWall) {
+        	var posPeix = parseInt($("#square").offset().top) + 25;
+        	if (posPeix >= botWall-25 || posPeix <= topWall+25) {
+        		$("#pointsDiv").hide();
+        		$("#game").hide();
+        		$("#endGame").show();
+        		$("#finalPuntuation").text(points);
+        	}
         }
     }
 
@@ -66,6 +68,6 @@ function getRandomArbitrary(min, max) {
 
 $(document).ready(function() {
     window.setInterval(function(){
-
-    }, 1000);
+    	acceleration += 1;
+    }, 4000);
 });
